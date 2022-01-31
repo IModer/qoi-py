11d5090aaeae4f643f5e20f110b0f5dd3af4c14c
IModer <theconectiongames@gmail.com>
2022-01-31 13:34:32 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,50 @@
+import qoi as q
+from sys import argv
+from PIL import Image
+
+def getColorspace(p):
+    pass
+
+def getMode(m):
+    if m == "RGBA":
+        return 4
+    elif m == "RGB":
+        return 3
+    else:
+        return 0
+
+def main():
+
+    filepath = ""
+    outpath  = ""
+    
+    #Open file
+    try:
+        f = Image.open(filepath)
+    except FileNotFoundError:
+        print("The file {} could not be found".format(filepath))
+        exit()
+    except:
+        print("The file {} could not be opened".format(filepath))
+        exit()
+
+
+    #colorspace = getColorspace(f)
+
+    mode = getMode(f.mode)
+
+    w, h = f.size
+    #                    colorspace hardcoded
+    desc = q.qoi_desc(w, h, mode, 1)
+
+    data = f.tobytes()
+
+    w = q.qoi_encode(data, desc)
+
+    f.close()
+
+    with open(outpath, "wb+") as f:
+        f.write(w)
+
+if __name__ == "__main__":
+    main()
